0beb1d84c648b10483f3f6e4c103051ad80aacaf
Khatisani Mongwe <khatisanimongwe@gmail.com>
2026-06-25 20:56:58 +0200

    Feat: Implement k-mer frequency calculation function

--- pipeline/transform_clean.py
+++ pipeline/transform_clean.py
@@ -4,3 +4,17 @@ def generate_kmer_space(k):
     bases = ['A', 'T', 'C', 'G']
     kmers = [''.join(p) for p in itertools.product(bases, repeat=k)]
     return {kmer: i for i, kmer in enumerate(kmers)}
+
+def sequence_to_kmer_frequencies(sequence, kmer_dict, k):
+    frequencies = [0.0] * len(kmer_dict)
+    num_kmers = len(sequence) - k + 1
+    
+    if num_kmers <= 0:
+        return frequencies
+        
+    for i in range(num_kmers):
+        kmer = sequence[i:i+k]
+        if kmer in kmer_dict:
+            frequencies[kmer_dict[kmer]] += 1.0
+            
+    return [count / num_kmers for count in frequencies]
