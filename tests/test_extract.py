4388eececf47ba410761f360d78c50e2dac6ad50
Khatisani Mongwe <khatisanimongwe@gmail.com>
2026-06-29 17:47:10 +0000

    test: test invalid file format should not be processed and files to be extracted must exist.

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from pipeline.extract import check_validity
 
@@ -10,6 +11,30 @@ class TestExtraction(unittest.TestCase):
             check_validity(actual_path)
         except SystemExit:
             self.fail(f"check_validity unexpectedly failed on your real file at: {actual_path}")
+
+    def test_file_does_not_exist(self):
+        missing_path = "data/nonexistent.fastq"
+        
+        try:
+            check_validity(missing_path)
+            self.fail("check_validity should have exited on a missing file, but it didn't!")
+        except SystemExit as error:
+            self.assertEqual(error.code, 1)
+
+    def test_invalid_file_extension(self):
+        bad_path = "data/example.txt"
+        
+        with open(bad_path, 'w') as f:
+            f.write("This is not genomic data.")
+            
+        try:
+            check_validity(bad_path)
+            self.fail("check_validity should have exited on a .txt file, but it didn't!")
+        except SystemExit as error:
+            self.assertEqual(error.code, 1)
+        finally:
+            if os.path.exists(bad_path):
+                os.remove(bad_path)
 
 if __name__ == "__main__":
     unittest.main()
