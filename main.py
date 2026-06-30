e0ee971956ad7c58156bcdd707026b7f7c5001ad
Khatisani <khatisanimongwe@gmail.com>
2026-06-30 02:51:36 +0200

    refine: link extraction and transformation phases into master pipeline orchestrator

--- main.py
+++ main.py
@@ -3,29 +3,55 @@ import sys
 import subprocess
 
 def main():
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 2:
         print("Usage Error!")
-        print("Usage: python3 main.py <inputfile.fastq> <outputfile.npy>")
+        print("Usage: python3 main.py <inputfile.fastq>")
         sys.exit(1)
         
     input_file = sys.argv[1]
-    output_file = sys.argv[2]
     
-    extracted_stage = "data/extracted_stage.tmp"
+    staging_file= "data/extracted_stage.tmp"
+
+    print("\n==================================================")
+    print("🧬 Starting Genomic-ETL Pipeline Engine 🧬")
+    print("==================================================\n")
+
+# --------------------------------------------------
+# STAGE 1: EXTRACTION & VALIDATION
+# --------------------------------------------------
     
-    print("==================================================")
-    print("Starting Genomic-ETL Pipeline Engine 🧬")
-    print("==================================================")
-  
-    print("\n[STEP 1/3] Launching Extraction ...")
-    extract_proc = subprocess.run(["python3", "extract.py", input_file])
+    print("\n--------------------------------------------------")
+    print("[STAGE 1] Running Extraction Engine...")
+    print("--------------------------------------------------\n")
+
+    extract_proc = subprocess.run(["python3", "pipeline/extract.py", input_file], capture_output=False)
     
     if extract_proc.returncode != 0:
         print("Pipeline failed at Stage 1: Extraction aborted.")
-        sys.exit(1)
-        
+        sys.exit(extract_proc.returncode) 
+    print("\n---------------Stage 1 complete.------------------\n")
+
+# --------------------------------------------------
+# STAGE 2: QUALITY TRANSFORMATION & FILTERING
+# --------------------------------------------------
+
+    print("\n--------------------------------------------------")
+    print("[STAGE 2] Running Transformation Engine...")
+    print("--------------------------------------------------\n")
+
+    trans_process = subprocess.run(
+        ["python3", "pipeline/transform.py", staging_file],
+        capture_output=False
+    )
+
+    if trans_process.returncode != 0:
+        print("\n❌ Pipeline aborted: Stage 2 Transformation failed.")
+        sys.exit(trans_process.returncode)
+
+    print("\n---------------Stage 2 complete.------------------\n")
+
     print("\n==================================================")
-    print("🎉 Genomic-ETL Pipeline Successfully Executed!")
+    print("Genomic-ETL Pipeline Successfully Executed!")
     print("==================================================\n")
 
 if __name__ == "__main__":
